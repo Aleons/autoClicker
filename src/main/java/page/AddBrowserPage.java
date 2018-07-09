50a928aad5debfbd27e8f394d37909ca74ce3d45
Aleons <thealeons@yandex.ru>
2018-07-10 01:14:00 +0300

    add mail click pom suite key exit

--- src/main/java/page/AddBrowserPage.java
+++ src/main/java/page/AddBrowserPage.java
@@ -10,9 +10,11 @@ public class AddBrowserPage {
 
     SelenideElement key = $(By.xpath("//*[@name=\"pass1\"]"));
 
-    public void addBrowser(){
+    public void addBrowser() throws InterruptedException {
         logInPage.openSite().sendLogin(System.getProperty("LOGIN")).sendPassword(System.getProperty("PASS")).clickEnterBtn();
         key.sendKeys(System.getProperty("KEY"));
         key.pressEnter();
+        Thread.sleep(2000);
+        $(By.xpath("/html/body/div[2]/div/table/tbody/tr/td/table[3]/tbody/tr/td[3]/nobr/a[2]")).pressEnter();
     }
 }
